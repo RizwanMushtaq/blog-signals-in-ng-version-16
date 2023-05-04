0a1b5263846f38c10af1d64bd7538d07270a54d0
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2023-05-04 14:44:29 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'blog-signals-in-ng-version-16';
+}
